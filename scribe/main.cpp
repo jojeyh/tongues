4282bedb0eba11077a2ebf975b6d954fc8cf1f0a
George Balch <george.balch@proton.me>
2024-05-30 02:41:42 -0700

    Write to socke t instead of local of file

--- scribe/main.cpp
+++ scribe/main.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 #include <string>
 
@@ -9,7 +13,18 @@
 static pa_mainloop *mainloop = NULL;
 static pa_context *context = NULL;
 static pa_stream *stream = NULL;
-static FILE *output_file = NULL;
+// static FILE *output_file = NULL;
+static int sockfd;
+
+void handle_interrupt(int signal) {
+    if (signal == SIGINT) {
+        if (mainloop) {
+            pa_mainloop_quit(mainloop, 0);
+        } else {
+            exit(0);
+        }
+    }
+}
 
 void stream_state_callback(pa_stream *s, void *userdata) {
     pa_stream_state_t state = pa_stream_get_state(s);
@@ -38,7 +53,14 @@ void stream_read_callback(pa_stream *s, size_t length, void *userdata) {
         fprintf(stderr, "pa_stream_peek() failed: %s\n", pa_strerror(pa_context_errno(context)));
         return;
     }
-    fwrite(data, 1, length, output_file);
+    if (s) {
+        write(sockfd, data, length);
+    }
+    // fwrite(data, 1, length, output_file);
+    // char buf[1024];
+    // read(sockfd, buf, sizeof(buf));
+    // printf("%s\n", buf);
+
     pa_stream_drop(s);
 }
 
@@ -46,10 +68,10 @@ void server_info_callback(pa_context *c, const pa_server_info *info, void *userd
     printf("Default sink name: %s\n", info->default_sink_name);
 
     pa_sample_spec ss;
-    ss.format = PA_SAMPLE_FLOAT32LE;
+    ss.format = PA_SAMPLE_S16LE;
     // TODO check for available freqs and if possible assign to 16000
     ss.rate = 16000;
-    ss.channels = 2;
+    ss.channels = 1;
 
     stream = pa_stream_new(c, "Record", &ss, NULL);
     if (!stream) {
@@ -113,17 +135,46 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    output_file = fopen("output.pcm", "wb");
-    if (!output_file) {
-        fprintf(stderr, "Failed to open output file\n");
+    // output_file = fopen("output.pcm", "wb");
+    // if (!output_file) {
+    //     fprintf(stderr, "Failed to open output file\n");
+    //     return 1;
+    // }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
         return 1;
     }
 
-    int ret = 0;
-    pa_mainloop_run(mainloop, &ret);
+    // Server address configuration
+    struct sockaddr_in s_addr;
+    s_addr.sin_family = AF_INET;             // IPv4
+    s_addr.sin_port = htons(43007); 
+    if (inet_pton(AF_INET, "127.0.0.1", &s_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+ 
+    int status;
+    if ((status
+         = connect(sockfd, (struct sockaddr*)&s_addr,
+                   sizeof(s_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
 
-    pa_context_unref(context);
-    pa_mainloop_free(mainloop);
+    signal(SIGINT, handle_interrupt);
+
+    int ret = pa_mainloop_run(mainloop, &ret);
+
+    if (ret) {
+        pa_context_unref(context);
+        pa_mainloop_free(mainloop);
+    }
 
     return ret;
 }
