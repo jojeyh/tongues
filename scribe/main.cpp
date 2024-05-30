6bdb350fc70ad7214c7ce4ca6d138925c13e9896
George Balch <george.balch@proton.me>
2024-05-29 20:26:09 -0700

    Rewritten main.cpp file

--- scribe/main.cpp
+++ scribe/main.cpp
@@ -1,155 +1,128 @@
+#include <pulse/pulseaudio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <signal.h>
 
 #include <string>
-#include <fstream>
-#include <iostream>
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
 
-#include <pulse/pulseaudio.h>
+// TODO should probably remove these from global context
+static pa_mainloop *mainloop = NULL;
+static pa_context *context = NULL;
+static pa_stream *stream = NULL;
+static FILE *output_file = NULL;
 
-#define BUFSIZE 1024
-
-std::ofstream pcm_file;
-
-// WAV file header structure
-// struct wav_header {
-//     char riff[4];            // "RIFF" chunk descriptor
-//     unsigned int file_size;  // File size excluding header
-//     char wave[4];            // "WAVE" chunk descriptor
-//     char fmt[4];             // "fmt " sub-chunk descriptor
-//     unsigned int fmt_size;   // Size of the fmt chunk
-//     unsigned short format;   // Format (PCM = 1)
-//     unsigned short channels; // Number of channels (mono = 1, stereo = 2)
-//     unsigned int sample_rate;// Sample rate (e.g., 44100)
-//     unsigned int byte_rate;  // Byte rate (sample_rate * channels * bits_per_sample / 8)
-//     unsigned short block_align; // Block align (channels * bits_per_sample / 8)
-//     unsigned short bits_per_sample; // Bits per sample (e.g., 16)
-//     char data[4];            // "data" sub-chunk descriptor
-//     unsigned int data_size;  // Size of the data chunk
-// };
-
-// // Write WAV header
-// // TODO these values should be declared in seperate struct, loser!
-// struct wav_header HEADER = {
-//     .riff = {'R', 'I', 'F', 'F'},
-//     .wave = {'W', 'A', 'V', 'E'},
-//     .fmt = {'f', 'm', 't', ' '},
-//     .fmt_size = 16, // Size of the fmt sub-chunk
-//     .format = 1,    // PCM format
-//     .channels = 2,
-//     .sample_rate = 48000U,
-//     .bits_per_sample = 16U, // Fixed 16 bits per sample for this example
-//     .byte_rate = 48000U * 2U * 16U / 8U,
-//     .block_align = 2 * 16 / 8,
-//     .data = {'d', 'a', 't', 'a'},
-//     .data_size = 0 // Placeholder for now, will be updated later
-// };
-
-void stream_state_cb(pa_stream *stream, void *userdata) {
-  const pa_stream_state state = pa_stream_get_state(stream);
+void stream_state_callback(pa_stream *s, void *userdata) {
+    pa_stream_state_t state = pa_stream_get_state(s);
     switch (state) {
-    case PA_STREAM_FAILED:
-      std::cout << "Stream failed\n";
-      break;
-    case PA_STREAM_READY:
-      std::cout << "Stream ready\n";
-
-      break;
-    default:
-      std::cout << "Stream state: " << state << std::endl;
+        case PA_STREAM_CREATING:
+            printf("Stream is creating\n");
+            break;
+        case PA_STREAM_READY:
+            printf("Stream is ready\n");
+            break;
+        case PA_STREAM_TERMINATED:
+            printf("Stream terminated\n");
+            break;
+        case PA_STREAM_FAILED:
+            printf("Stream failed: %s\n", pa_strerror(pa_context_errno(context)));
+            break;
+        default:
+            printf("Stream state: %d\n", state);
+            break;
     }
 }
 
-void pa_stream_read_cb(pa_stream *stream, const size_t /*nbytes*/, void* /*userdata*/)
-{
-    // Careful when to pa_stream_peek() and pa_stream_drop()!
-    // c.f. https://www.freedesktop.org/software/pulseaudio/doxygen/stream_8h.html#ac2838c449cde56e169224d7fe3d00824
-    int16_t *data = nullptr;
-    size_t actualbytes = 0;
-    if (pa_stream_peek(stream, (const void**)&data, &actualbytes) != 0) {
-        std::cerr << "Failed to peek at stream data\n";
+void stream_read_callback(pa_stream *s, size_t length, void *userdata) {
+    const void *data;
+    if (pa_stream_peek(s, &data, &length) < 0) {
+        fprintf(stderr, "pa_stream_peek() failed: %s\n", pa_strerror(pa_context_errno(context)));
+        return;
+    }
+    fwrite(data, 1, length, output_file);
+    pa_stream_drop(s);
+}
+
+void server_info_callback(pa_context *c, const pa_server_info *info, void *userdata) {
+    printf("Default sink name: %s\n", info->default_sink_name);
+
+    pa_sample_spec ss;
+    ss.format = PA_SAMPLE_S16LE;
+    ss.rate = 48000;
+    ss.channels = 2;
+
+    stream = pa_stream_new(c, "Record", &ss, NULL);
+    if (!stream) {
+        fprintf(stderr, "pa_stream_new() failed: %s\n", pa_strerror(pa_context_errno(c)));
+        pa_mainloop_quit(mainloop, 1);
         return;
     }
 
-    if (data == nullptr && actualbytes == 0) {
-      // No data in the buffer, ignore.
-      return;
-    } else if (data == nullptr && actualbytes > 0) {
-        // Hole in the buffer. We must drop it.
-        if (pa_stream_drop(stream) != 0) {
-            std::cerr << "Failed to drop a hole! (Sounds weird, doesn't it?)\n";
-            return;
-        }
-    }
+    pa_stream_set_state_callback(stream, stream_state_callback, NULL);
+    pa_stream_set_read_callback(stream, stream_read_callback, NULL);
 
-    // process data
-    pcm_file.write((const char*)data, actualbytes);
-    pcm_file.flush();
-
-    if (pa_stream_drop(stream) != 0) {
-      std::cerr << "Failed to drop data after peeking.\n";
-    }
-}
-
-void server_info_cb(pa_context *ctx, const pa_server_info *info, void *userdata) {
-    std::cout << "Default sink: " << info->default_sink_name << std::endl;
-
-    pa_sample_spec spec;
-    spec.format = PA_SAMPLE_S16LE;
-    spec.rate = 48000;
-    spec.channels = 2;
-    // Use pa_stream_new_with_proplist instead?
-    pa_stream *stream = pa_stream_new(ctx, "output monitor", &spec, nullptr);
-
-    pa_stream_set_state_callback(stream, &stream_state_cb, nullptr /*userdata*/);
-    pa_stream_set_read_callback(stream, &pa_stream_read_cb, nullptr /*userdata*/);
+    pa_buffer_attr buffer_attr;
+    buffer_attr.maxlength = (uint32_t) -1;
+    buffer_attr.tlength = (uint32_t) -1;
+    buffer_attr.prebuf = (uint32_t) -1;
+    buffer_attr.minreq = (uint32_t) -1;
 
     std::string monitor_name(info->default_sink_name);
     monitor_name += ".monitor";
-    if (pa_stream_connect_record(stream, monitor_name.c_str(), nullptr, PA_STREAM_NOFLAGS) != 0) {
-      std::cerr << "connection fail\n";
-      return;
+    if (pa_stream_connect_record(stream, monitor_name.c_str(), &buffer_attr, PA_STREAM_NOFLAGS) < 0) {
+        fprintf(stderr, "pa_stream_connect_record() failed: %s\n", pa_strerror(pa_context_errno(c)));
+        pa_mainloop_quit(mainloop, 1);
     }
-
-    std::cout << "Connected to " << monitor_name << std::endl;
 }
 
-void context_state_callback(pa_context *ctx, void *userdata) {
-    const pa_context_state state = pa_context_get_state(ctx);
+void context_state_callback(pa_context *c, void *userdata) {
+    pa_context_state_t state = pa_context_get_state(c);
     switch (state) {
-    case PA_CONTEXT_READY:
-      std::cout << "Context ready\n";
-      pa_context_get_server_info(ctx, &server_info_cb, nullptr /*userdata*/);
-      break;
-    case PA_CONTEXT_FAILED:
-      std::cout << "Context failed\n";
-      break;
-    default:
-      std::cout << "Context state: " << state << std::endl;
+        case PA_CONTEXT_READY: {
+            printf("Context is ready\n");
+
+            pa_operation *o = pa_context_get_server_info(c, server_info_callback, NULL);
+            if (o) {
+                pa_operation_unref(o);
+            }
+
+            
+            break;
+        }
+        case PA_CONTEXT_TERMINATED:
+            printf("Context terminated\n");
+            pa_mainloop_quit(mainloop, 0);
+            break;
+        case PA_CONTEXT_FAILED:
+            printf("Context failed: %s\n", pa_strerror(pa_context_errno(c)));
+            pa_mainloop_quit(mainloop, 1);
+            break;
+        default:
+            printf("Context state: %d\n", state);
+            break;
     }
 }
 
-int
-main (int    argc,
-      char **argv)
-{
-    pcm_file.open("captured_audio.pcm", std::ios::binary);
+int main(int argc, char *argv[]) {
+    mainloop = pa_mainloop_new();
+    context = pa_context_new(pa_mainloop_get_api(mainloop), "Tongues");
 
-    pa_mainloop *loop = pa_mainloop_new();
-    pa_mainloop_api *loop_api = pa_mainloop_get_api(loop);
-    pa_context *ctx = pa_context_new(loop_api, "tongues");
-    pa_context_set_state_callback(ctx, &context_state_callback, nullptr);
-    if (pa_context_connect(ctx, nullptr, PA_CONTEXT_NOFLAGS, nullptr) < 0) {
-      std::cerr << "PA connection failed.\n";
-      return 1;
+    pa_context_set_state_callback(context, context_state_callback, NULL);
+    if (pa_context_connect(context, NULL, PA_CONTEXT_NOFLAGS, NULL) < 0) {
+        fprintf(stderr, "pa_context_connect() failed: %s\n", pa_strerror(pa_context_errno(context)));
+        return 1;
     }
 
-    pa_mainloop_run(loop, nullptr);
+    output_file = fopen("output.pcm", "wb");
+    if (!output_file) {
+        fprintf(stderr, "Failed to open output file\n");
+        return 1;
+    }
 
-    return 0;
+    int ret = 0;
+    pa_mainloop_run(mainloop, &ret);
+
+    pa_context_unref(context);
+    pa_mainloop_free(mainloop);
+
+    return ret;
 }
